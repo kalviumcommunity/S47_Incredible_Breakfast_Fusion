a49ce5a176355d89d6502d85b0b852c30bf3ece2
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-01-30 14:25:31 +0530

    Created a react app in my client folder

---
+++ client/src/page/home.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <div className='flex navbar'>
+            <h1>Incredible Breakfast Fusion</h1>
+            <div className='flex navbar_side'>
+            <h3 className='active'>Home</h3>
+            <h3 className='active'>About</h3>
+            </div>
+        </div>
+        <div className="grid-cont">
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4 >weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+                <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+            <div className='box box1'>
+                <div className='img'>
+                    <h2>Img</h2>
+                </div>
+                <h4>weird combo</h4>
+                <h5>Description</h5>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
