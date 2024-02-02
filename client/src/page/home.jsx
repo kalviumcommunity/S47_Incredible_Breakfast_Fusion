3f6d9ce54e8290f37cdc378114e61a37fda5fb15
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-02-02 14:38:22 +0530

    create a component and render dummy data

--- client/src/page/home.jsx
+++ client/src/page/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./home.css"
+import data from "../data.json"
 
 const Home = () => {
   return (
@@ -7,19 +8,28 @@ const Home = () => {
         <div className='flex navbar'>
             <h1>Incredible Breakfast Fusion</h1>
             <div className='flex navbar_side'>
-            <h3 className='active'>Home</h3>
-            <h3 className='active'>About</h3>
+                <h3 className='active'>Home</h3>
+                <h3 className='active'>About</h3>
             </div>
         </div>
         <div className="grid-cont">
-            <div className='box box1'>
-                <div className='img'>
-                    <h2>Img</h2>
-                </div>
-                <h4 >weird combo</h4>
-                <h5>Description</h5>
-            </div>
-            <div className='box box1'>
+            
+                {data.map((data) => (
+                    <>
+                    <div className='box box1'>
+                    <div className='img'>
+                        <img src={data.image} alt=""/>
+                    </div>
+                    <div>
+                        <h4 >{data.weird_combo}</h4>
+                        <h5>{data.description}</h5>
+                    </div>
+                    </div>
+                    </>    
+                ))}
+                
+            
+            {/* <div className='box box1'>
                 <div className='img'>
                     <h2>Img</h2>
                 </div>
@@ -74,7 +84,7 @@ const Home = () => {
                 </div>
                 <h4>weird combo</h4>
                 <h5>Description</h5>
-            </div>
+            </div> */}
         </div>
     </div>
   )
