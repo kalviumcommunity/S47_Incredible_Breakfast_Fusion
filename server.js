6bba464fe1efea9aae85368659c6874bc8d20bb0
anushkapoonia07 <anushkapoonia53@gmail.com>
2024-02-13 15:58:03 +0530

    Update the auth endpoints to use JWT token and Store the token in the cookie

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 const FoodModel = require('./index')
 const port = 3000;
 const joi = require('joi')
+const jwt=require('jsonwebtoken')
 
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 const updateSchema=joi.object({
   FoodItem1:joi.string().required(),
@@ -62,7 +64,31 @@ app.post('/Weird_combos', (req,res)=>{
       FoodModel.findByIdAndUpdate({ _id: id }, { FoodItem1, FoodItem2, WeirdCombos, Description }, { new: true })
       .then(food => res.json(food))
       .catch(err => console.log(err))
-    })    
+    })  
+    
+    app.post('/login', (req, res) => {
+      const { email, username } = req.body;
+  
+      const token = jwt.sign({ email }, JWT_SECRET_KEY, { expiresIn: '1h' });
+      res.cookie('token', token, { httpOnly: true });
+      res.json({ success: true });
+  });
+  
+  function authenticateToken(req, res, next) {
+    const token = req.cookies.token;
+  
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized: Missing token' });
+    }
+  
+    jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Forbidden: Invalid token' });
+        }
+        req.user = decoded;
+        next();
+    });
+  }
   })
   .catch(err => {
     console.error('Error connecting to MongoDB Atlas', err);
